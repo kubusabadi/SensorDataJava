dbca5a4894ebbe77f5ac02beb7b2e64a8285bafa
kubusabadi <kubusabadi@hotmail.com>
2021-06-12 22:44:48 +0200

    spring boot app created

---
+++ src/main/java/com/kubusabadi/SensorData/SensorDataApplication.java
@@ -0,0 +1,13 @@
+package com.kubusabadi.SensorData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubusabadi/SensorData/SensorDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubusabadi.SensorData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
